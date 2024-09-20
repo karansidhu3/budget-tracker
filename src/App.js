6dea4f5639c57bce648a04dc5efd9df1fe610256
Karanveer Sidhu <karansidhu5550@gmail.com>
2024-09-20 12:23:20 -0700

    created api

--- src/App.js
+++ src/App.js
@@ -7,8 +7,18 @@ function App() {
   const [datetime, setDateTime] = useState('');
   const [description, setDescription] = useState('');
 
-  function addNewTransaction(){
-    
+  function addNewTransaction(ev){
+    ev.preventDefault();
+    const url = process.env.REACT_APP_API_URL + '/transaction';
+    fetch(url, {
+      method: 'POST',
+      headers: {'Content-type': 'application/json'},
+      body: JSON.stringify({name, description, datetime})
+    }).then(res => {
+      res.json().then((json) =>{
+        console.log('result', json);
+      })
+    })
   }
 
 
