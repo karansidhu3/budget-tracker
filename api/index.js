fbf99c61c1459d5658e7073497731e23f5da3f13
Karanveer Sidhu <karansidhu5550@gmail.com>
2024-09-20 16:24:38 -0700

    made and connected to database, now program can get/post info in database

--- api/index.js
+++ api/index.js
@@ -1,12 +1,35 @@
 const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const Transaction = require('./models/Transaction.js');
+const { default: mongoose } = require('mongoose');
 const app = express();
+
+app.use(cors());
+app.use(express.json());
 
 app.get('/api/test', (req, res) => {
   res.json('test ok2');
 });
 
-app.post('/api/transaction', (req,res) => {
-  res.json(req.body);
+app.post('/api/transaction', async(req,res) => {
+  // connect to the database
+  await mongoose.connect(process.env.MONGO_URL);
+  // get info out of req post body
+  const {price, name, description, datetime} = req.body;
+  // use transaction model to format info
+  const transaction = await Transaction.create({price, name, description, datetime});
+  // post data
+  res.json(transaction);
 })
+
+app.get('/api/transactions', async(req,res) => {
+  // connect to db
+  await mongoose.connect(process.env.MONGO_URL);
+  // get all records in db
+  const transactions = await Transaction.find();
+  // give data back to user
+  res.json(transactions);
+});
 
 app.listen(4040);
